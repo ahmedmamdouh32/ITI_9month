93109d0f35fad69bc39978bc4d07e2aaeff9c57b
ahmed_mamdouh <ahmedmamdouh.am32@gmail.com>
2025-11-10 22:13:26 +0200

    adding day 3 database

--- C++/Day10/lab/C++exam/main.cpp
+++ C++/Day10/lab/C++exam/main.cpp
@@ -2,25 +2,51 @@
 
 using namespace std;
 
+class parent{
+    int age;
 
 
-class ab {
 public :
-ab(){
 
-cout<<"any";
+
+    parent(){
+        age = 50;
+        cout<<"Iam the default constructor"<<endl;
+    }
+
+    parent(parent& P1){
+        age = P1.age;
+        cout<<"Iam the copy constructor"<<endl;
+    }
+
+
+void setAge(int n){
+    age = n;
+}
+int getAge(){
+    return age;
 }
 
-virtual void print() = 0; //pure virtual function declaration
-
 };
-int main()
-{
 
-ab a;
+void show(parent& p){
+    cout<<"Iam the show function"<<p.getAge()<<endl;
+}
+
+int main(){
+
+parent P1;
+//parent P2 = P1;
+show(P1);
+
+
+
+
+
+
+
 
 
 return 0;
+
 }
-
-
