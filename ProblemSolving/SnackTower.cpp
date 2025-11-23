61f24237a33e7a0f3acb4d42df2b3d6beb5bca8c
ahmed_mamdouh <ahmedmamdouh.am32@gmail.com>
2025-11-24 00:41:56 +0200

    adding the last problem solving task of the day

---
+++ ProblemSolving/SnackTower.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	vector<int> availableSnacksSize;
+	int n;
+	cin >> n;
+	int currentMaxSize = n;
+	int dailySnackSize;
+	for (int i = 0; i < n; i++) {
+		cin >> dailySnackSize;
+		if (dailySnackSize == currentMaxSize){
+			cout << dailySnackSize << " ";
+			currentMaxSize--;
+			while (currentMaxSize != 0) {
+				auto it = find(availableSnacksSize.begin(), availableSnacksSize.end(), currentMaxSize);
+				if (it != availableSnacksSize.end()) {
+					cout << currentMaxSize << " ";
+					currentMaxSize--;
+				}
+				else
+				{
+					break;
+				}
+			}
+			cout << endl;
+		}
+		else{
+			cout << endl;
+			availableSnacksSize.push_back(dailySnackSize);
+		}
+	}
+    return 0;
+}
