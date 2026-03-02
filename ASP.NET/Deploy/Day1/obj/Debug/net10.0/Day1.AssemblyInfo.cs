c9c4b807184e0b5d2a4add5755ecf2c76b9852e6
ahmed_mamdouh <ahmedmamdouh.am32@gmail.com>
2026-03-02 04:01:26 +0200

    adding latest files

--- ASP.NET/Deploy/Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
+++ ASP.NET/Deploy/Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+085323375470120aa2deb7556448dbf06ec1a923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036477c7430516977b27217d04c39b7d76878472")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
